e9592c4e5abfb494feca1cab8441b682eaab7deb
Enes T <125091714+enes-th@users.noreply.github.com>
2023-04-08 16:45:48 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+/**
+ * Intellectual property of KESENCY. 
+ * 
+ * 
+ * @author KESENCY LLC
+ * @link https://kesency.com/
+ * @version 1.0.1
+ * @license MIT
+ * 
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
